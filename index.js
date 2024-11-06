e9d720e61bd8168177a7102ffb4ae06041f8e22e
Giyoung Lim <gylim7826@naver.com>
2024-11-06 13:54:57 +0900

    '중간수정'

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
@@ -10,5 +18,17 @@ mongoose
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hello World! I'm muscle"));
+
+app.post("/register", async (req, res) => {
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+    return res.status(200).json({ success: true });
+  } catch (err) {
+    return res.json({ success: false, err });
+  }
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
